085215c7aa9551f9332531b030a2f4518a502048
elaine_tea <elaine_tea@tuta.io>
2026-09-23 17:18:56 +0200

    solved first 5 exercises

---
+++ lab2/Reusable/Exercises/2-const.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Define constant to store your birth year as a number
+
+const year = 1990;
+
+module.exports = { year };
---
+++ lab2/Reusable/Exercises/5-range-odd.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// Implement function `rangeOdd(start: number, end: number)` returning
+// array with all odd numbers from the range [15, 30] including endpoints
+
+const rangeOdd = (start, end) => {
+  if (end - start < 0) return [];
+  const array = [];
+  let i = 0;
+  for (let n = start; n <= end; n++) {
+    if (n % 2 !== 0) {
+      array[i] = n;
+      i++;
+    }
+  }
+  return array;
+};
+
+module.exports = { rangeOdd };
