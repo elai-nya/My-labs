15f910a4ab82fcef12c95407125b85ae515ff6a7
elaine_tea <elaine_tea@tuta.io>
2026-09-23 15:51:26 +0200

    added first lab

---
+++ lab1/dataTypes.js
@@ -0,0 +1,7 @@
+const arrey = [false, "buy", 3.14, "true", 15, true, null, Infinity];
+
+for (let index = 0; index < arrey.length; index++) {
+  const element = arrey[index];
+
+  console.log(typeof element);
+}
